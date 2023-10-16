314a35e753f5e657df590c9b01a6e2039ed0cb96
unknown <felixtorresjavierantonio@gmail.com>
2023-10-15 23:57:37 -0600

    update

--- src/views/MusicList.vue
+++ src/views/MusicList.vue
@@ -6,8 +6,8 @@
  
  <script lang="ts">
  import MusicList from '../components/MusicList.vue';
-import { useMusicList } from '../stores/musicList';
- 
+ import { useMusicList } from '../stores/musicList';
+
  import { defineComponent, ref } from 'vue';
  
  export default defineComponent({
@@ -21,6 +21,25 @@ import { useMusicList } from '../stores/musicList';
     music.$subscribe((mutation, state) => {
       musicList.value = state.musicListState
     })
+
+    // API for get requests 
+    let fetchRes = fetch( 
+    "http://127.0.0.1:2222/api/songs"); 
+    // fetchRes is the promise to resolve 
+    // it by using.then() method 
+    fetchRes.then(res => 
+        res.json()).then(d => { 
+            console.log(d) 
+        }) 
+
+ /*    async function getUser() {
+  try {
+    const response = await axios.get('/user?ID=12345');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+} */
     return {musicList}
    }
  });
