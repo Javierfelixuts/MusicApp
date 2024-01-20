6f6c45b2ef9e80d996fd73fd406e34af53dca7d2
unknown <felixtorresjavierantonio@gmail.com>
2024-01-19 20:19:01 -0700

    UPDATE

--- src/views/MusicList.vue
+++ src/views/MusicList.vue
@@ -1,11 +1,21 @@
 <template>
-    <div>
-        <MusicList :musicList="musicServerList"  />
+  <div id="main" :class="{ 'grid gridCols1 sm:grid-cols-2 md:grid-cols-12 gap-x-6 gap-y-5': true }"
+        class="main mb-12 mb:mb-80">
+      <div class="col-start-1 col-end-5 overflow-y-scroll h-4/5">
+          <MusicList :musicList="musicServerList"  />
+      </div>
+    <div class="col-start-5 col-end-13 mx-3">
+        <div >
+            <!-- Reproductor -->
+            <MusicPlayer  />
+        </div>
     </div>
+  </div>
  </template>
  
  <script lang="ts">
  import MusicList from '../components/MusicList.vue';
+import MusicPlayer from '../components/MusicPlayer.vue';
  import { useMusicList } from '../stores/musicList';
  import { useSongsServerStore } from '../stores/musicServerStore';
  import axios from 'axios';
@@ -13,8 +23,9 @@
  
  export default defineComponent({
    components: {
-     MusicList,
-   },
+    MusicList,
+    MusicPlayer
+},
    setup(){
     const music = useMusicList();
     const musicList = ref(music.musicListState);
