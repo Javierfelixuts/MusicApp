25870cdcc7d7199f2058ea1cc8a75df97149082b
unknown <felixtorresjavierantonio@gmail.com>
2024-04-10 20:35:34 -0700

    UPDATE  menu

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="menuRight" class="text-white p-2 absolute w-1/2 h-full bg-slate-100 z-20 rounded-sm right-0 top-20">
+    <div id="menuRight" class="text-white p-2 absolute w-500 h-full bg-slate-100 z-20 rounded-sm right-0 top-20">
         <h1 class="text-black">Configuración</h1>
         <div class="flex items-center">
             <p class="text-black text-xs">Cambiar color</p>
@@ -42,6 +42,8 @@ export default {
         const router = useRouter();
         const local = new LocalStorageManager();
         const useChangeColor = useChangeHeaderColor();
+
+        
         
 
         const clearColor = () => {
