529e499654f80058a9140894051e55bed1e9d399
unknown <felixtorresjavierantonio@gmail.com>
2024-06-09 21:53:56 -0700

    UPDATE

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,24 +1,29 @@
 <template>
-    <div>
+    <div :key="album.id" @click="getAlbumId(album.id)">
         <img class="object-cover h-28 w-28 m-2 cursor-pointer" :src="album.image" alt="" srcset="">
-        <h6 class="text-xs hidden">Carlos Rivera - "El Rey"</h6>
+        <h6 class="text-xs">{{ album.name }}</h6>
     </div>
 </template>
 
 <script  lang="ts">
+import { Album } from '../types/Album';
+
 export default {
     name: "Album",
     props:{
         album:{
-            name: String,
-            image: String,
+            type: Object as () => Album,
+            required: true,
 
-        }
+        },
     },
-    setup(props){
-        console.log(props.album.image);
+    setup( props, context){
+        function getAlbumId(id: number){
+            context.emit('emitAlbumId', id);
+            console.log("id: ", id);
+        }
         return {
-            name: "hey"
+            getAlbumId,
         }
     }
 }
